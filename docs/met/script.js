f2b7633d64c0205684b754542bba7a6774923d8e
SUGIMOTO Tatsuo <sugi@zero-out.com>
2021-05-22 11:54:17 +0900

    img

--- docs/met/script.js
+++ docs/met/script.js
@@ -26,12 +26,12 @@ const search = async () => {
     list.appendChild(item);
   }
 
-  let i = 1;
+  let i = 0;
   for (let id of ids) {
     const objectUri = `${objectsUri}/${id}`;
     const objectJson = await getData(objectUri);
 
-    const img = document.querySelector(`.object img:nth-of-type(${i})`);
+    const img = document.querySelectorAll(`.object img`)[i];
     // const img = document.createElement('img');
     img.src = objectJson['primaryImageSmall'];
     // img.className = 'thumbnail';
@@ -66,42 +66,3 @@ const renderJson = (json) => {
   div.textContent = JSON.stringify(json, "", 2);
   document.getElementById('result').appendChild(div);
 }
-
-
-// const uri = 'https://script.google.com/macros/s/AKfycbxyacpN8y4nxSAnU0Eji6E_rBRDFTY7YoWWFa0clY5ELRhskgpt/exec';
-// const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
-// const sheet = 'Studio';
-// const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
-
-// const renderJson = (json) => {
-//   const studios = json.records;
-  
-//   studios.forEach(studio => {
-//    const studioDiv = document.createElement('div');
-//    const studioTitle = document.createElement("span");
-//    studioTitle.className = 'studio-title';
-//    studioTitle.textContent = studio['name-ja'];
-//    const studioTitleEn = document.createElement("span");
-//    studioTitleEn.className = 'studio-title-en';
-//    studioTitleEn.textContent = studio['name-en'];
-//    studioDiv.appendChild(studioTitle);
-//    studioDiv.appendChild(studioTitleEn);
-//    document.getElementById('studios').appendChild(studioDiv);
-//  });
-//   document.getElementById('result').textContent = JSON.stringify(json, null, 2);
-// }
-
-// const getData = async () => {
-//   try {
-//     const response = await fetch(endpoint);
-//     if (response.ok) {
-//       const jsonResponse = await response.json();
-// 			renderJson(jsonResponse);
-//     }
-//   }
-//   catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getData();
